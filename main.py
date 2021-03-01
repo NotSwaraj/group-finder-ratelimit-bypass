cf041b5e546c278c6759e9d22d1b955adad1363a
NotSwaraj <70449885+NotSwaraj@users.noreply.github.com>
2021-03-01 14:56:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while True:
 	r = requests.get(f"https://www.roblox.com/groups/group.aspx?gid={id}") # yes dont bully me it searches html which is not the best idea but it does its job lol
 	if 'owned' not in r.text: # from the html
 		re = requests.get(f"https://groups.roblox.com/v1/groups/{id}")
-		if 'isLocked' not in re.text:
+		if 'isLocked' not in re.text and 'owner' in re.text:
 			if re.json()['publicEntryAllowed'] == True and re.json()['owner'] == None:
 				print(f"\033[92m[ VALID ]https://www.roblox.com/groups/group.aspx?gid={id}\033[39m")	
 				requests.post("https://discord.com/api/webhooks/815709937258463293/QKOw0GCaPTYWp67wWEYUbRZvjToaYJQ6-CZriZvQ3Z3wGDWYftUSl1t7K3MJHdJ_4eda",json={'content':f'https://www.roblox.com/groups/group.aspx?gid={id}'})
