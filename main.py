5769eb0880f60c847942410632913b5b06684527
NotSwaraj <70449885+NotSwaraj@users.noreply.github.com>
2021-03-01 11:40:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os 
 try:
-	import requests, random, threading
+	import requests, random
 except ModuleNotFoundError:
 	os.system("pip install requests") # i swear requests is prebuit in tho 
-def main():
+while True:
 	id = random.randint(5000000, 8500000)
 	r = requests.get(f"https://www.roblox.com/groups/group.aspx?gid={id}") # yes dont bully me it searches html which is not the best idea but it does its job lol
 	if 'owned by' not in r.text: # from the html
@@ -19,5 +19,3 @@ def main():
 	else:
 		print(f"\033[91m[ INVALID ] > {id}\033[39m")	
 
-for i in range(50): # i know this says 50 but try not to edit this cause it will most likely get you ratelimited
-	threading.Thread(target=main).start()
