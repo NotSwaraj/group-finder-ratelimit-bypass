69681f2745479678cd3b3c47c2b067aa159e54b7
NotSwaraj <70449885+NotSwaraj@users.noreply.github.com>
2021-03-01 11:28:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
 			re = requests.get(f"https://groups.roblox.com/v1/groups/{id}")
 			if re.json()['publicEntryAllowed'] == True and re.json()['owner'] == None:
 				print(f"\033[92m[ VALID ]https://www.roblox.com/groups/group.aspx?gid={id}\033[39m")	
-				requests.post("https://discord.com/api/webhooks/815709937258463293/QKOw0GCaPTYWp67wWEYUbRZvjToaYJQ6-CZriZvQ3Z3wGDWYftUSl1t7K3MJHdJ_4eda",json={'content':f'https://www.roblox.com/groups/group.aspx?gid={id}'})
+				requests.post("Your_webhook_here",json={'content':f'https://www.roblox.com/groups/group.aspx?gid={id}'})
 			else:
 				print(f"\033[91m[ INVALID ] > {id}\033[39m")
 		else:
